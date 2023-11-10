af407a1f97ac0cc4c428b8efc435080d8d113306
Lefteris Gilmaz <lefterisgilmaz@Lefteriss-MBP.fritz.box>
2023-11-10 23:18:25 +0100

    refacors everything

--- Transaction.py
+++ Transaction.py
@@ -1,6 +1,5 @@
 import json
 
-
 class Transaction:
 
     transactions_list = []
@@ -20,8 +19,12 @@ class Transaction:
         }
         Transaction.transactions_list.append(transaction_record)
         
+    def serialize_transactions(self):
+        json_file = "data.json"
+        with open(json_file, "w") as write_file:
+            json.dump(self.transactions_list, write_file, indent=4, sort_keys=True)
 
-    
+            
     def search_transaction(self, search_date):
         filtered_by_date = list(filter(lambda x: x['date'] == search_date, self.transactions_list))
         return filtered_by_date
@@ -35,37 +38,8 @@ class Transaction:
     
     
 
-    def serialize_transactions(self):
-        with open("data.json", "w") as write_file:
-            json.dump(self.transactions_list, write_file, indent=4, sort_keys=True)
+
         
 
 
 
-with open("data.json", "r") as read_file:
-    existing_data = json.load(read_file)
-
-
-transaction_instance = Transaction(id=1, amount=15, date="2023-01-02", description="Sim Card")
-
-
-transaction_instance.transactions_list = existing_data
-
-
-print("All Transactions before Editing:")
-print(transaction_instance.get_transactions())
-
-
-edit_transaction_id = 2
-updated_transaction_data = {
-    'id': edit_transaction_id,
-    'amount': 50,
-    'date': '2023-01-05',
-    'description': 'Updated Groceries'
-}
-
-
-transaction_instance.edit_transaction(updated_transaction_data)
-
-print("\nAll Transactions after Editing:")
-print(transaction_instance.get_transactions())
--- main.py
+++ main.py
@@ -4,4 +4,33 @@ import json
 
 if __name__ == "__main__":
 
-    pass
+    with open("data.json", "r") as read_file:
+        existing_data = json.load(read_file)
+
+
+    transaction_instance = Transaction(id=1, amount=15, date="2023-01-02", description="Sim Card")
+
+
+    transaction_instance.transactions_list = existing_data
+
+
+    print("All Transactions before Editing:")
+    print(transaction_instance.get_transactions())
+
+
+    edit_transaction_id = 2
+    updated_transaction_data = {
+        'id': edit_transaction_id,
+        'amount': 50,
+        'date': '2023-01-05',
+        'description': 'Updated Groceries'
+    }
+
+    updated_transaction_data.serialize_transactions()
+
+
+    transaction_instance.edit_transaction(updated_transaction_data)
+
+    print("\nAll Transactions after Editing:")
+    print(transaction_instance.get_transactions())
+
