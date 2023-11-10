057d32075bd35af803293d256aeb503a9740e24b
Lefteris Gilmaz <lefterisgilmaz@lefteriss-mbp.speedport.ip>
2023-11-10 16:36:02 +0100

    adds id class atribute

--- Transaction.py
+++ Transaction.py
@@ -5,13 +5,15 @@ class Transaction:
 
     transactions_list = []
     
-    def __init__(self, amount, date, description):
+    def __init__(self, id, amount, date, description):
+        self.id = id
         self.amount = amount
         self.date = date
         self.description = description
 
     def add_transaction(self):
         transaction_record = {
+            'id': self.id,
             'amount': self.amount,
             'date': self.date,
             'description': self.description
@@ -19,20 +21,24 @@ class Transaction:
         Transaction.transactions_list.append(transaction_record)
         
 
-    @classmethod
-    def search_transaction(cls, search_date):
-        filtered_by_date = list(filter(lambda x: x['date'] == search_date, cls.transactions_list))
+    
+    def search_transaction(self, search_date):
+        filtered_by_date = list(filter(lambda x: x['date'] == search_date, self.transactions_list))
         return filtered_by_date
 
-        
-    @classmethod
-    def get_transactions(cls):
-        return cls.transactions_list
+    def edit_transaction(self):
+        pass
+
+
+
+
+    def get_transactions(self):
+        return self.transactions_list
     
     
-    @classmethod
-    def serialize_transactions(cls):
+
+    def serialize_transactions(self):
         with open("data.json", "w") as write_file:
-            json.dump(cls.transactions_list, write_file, indent=4)
+            json.dump(self.transactions_list, write_file, indent=4)
         
 
